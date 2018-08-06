7a88e584ffd5c87a4f4ddd6ec14e7e495b70dba8
Lucas Arnström <lucas@arnstrom.se>
2018-08-06 07:48:16 +0200

    changed way index page is rendered

--- src/Pages/Blog.elm
+++ src/Pages/Blog.elm
@@ -1,11 +1,17 @@
 module Pages.Blog exposing (..)
 
-import Html exposing (Html, text, h3, ul, li, button, div)
-import Html.Attributes
+import Html exposing (Html, text, h4, ul, li, button, div)
+import Html.Attributes exposing (href)
 import Html.Events exposing (onClick)
 import Http
 import Markdown
 import Bootstrap.Grid as Grid
+
+
+--import Bootstrap.Card as Card
+--import Bootstrap.Card.Block as Block
+--import Bootstrap.Button as Button
+
 import BlogDecoder exposing (Post, File, extractPostName)
 import RouteUrl.Builder exposing (Builder, builder, replacePath, path)
 import Maybe.Extra
@@ -112,36 +118,63 @@ update msg model =
 view : Model -> Html Msg
 view model =
     let
-        postname2item : Post -> Html Msg
-        postname2item post =
-            button [ onClick (FetchPost post.name) ] [ text <| extractPostName post ]
+        post2item : Post -> Html Msg
+        post2item post =
+            let
+                {- Capitilizes the first letter in a string.
+                   Example: capitilizeFirst "foobar" -> "Foobar"
+                -}
+                capitilizeFirst : String -> String
+                capitilizeFirst string =
+                    String.toUpper (String.left 1 string) ++ String.dropLeft 1 string
 
-        postnames2items : List Post -> List (Html Msg)
-        postnames2items postlist =
-            List.map postname2item postlist
+                {- Replaces all '_' with a space.
+                   Example: replaceUnderscores "asdf_foobar" -> "asdf foobar"
+                -}
+                replaceUnderscores : String -> String
+                replaceUnderscores string =
+                    String.split "_" string
+                        |> String.join " "
 
-        index : Model -> Html Msg
+                title =
+                    extractPostName post
+                        |> replaceUnderscores
+                        |> capitilizeFirst
+                        |> text
+            in
+                Grid.row []
+                    --[ Grid.col []
+                    --    [ Card.config []
+                    --        |> Card.block []
+                    --            [ Block.titleH4 [] [ text (extractPostName post) ]
+                    --            , Block.link [ href ("#!/blog/" ++ post.name) ] [ text "Open" ]
+                    --            ]
+                    --        |> Card.view
+                    --    ]
+                    --]
+                    [ Grid.col []
+                        [ h4 [] [ title ] ]
+                    ]
+
+        index : Model -> List (Html Msg)
         index model =
-            ul [] <|
-                if model.contentlist == [] then
-                    [ text "No blog posts loaded." ]
-                else
-                    postnames2items model.contentlist
+            if model.contentlist == [] then
+                [ Grid.row []
+                    [ Grid.col []
+                        [ text "No blog posts loaded." ]
+                    ]
+                ]
+            else
+                List.map post2item model.contentlist
     in
         case model.subpage of
             Index ->
-                Grid.row []
-                    [ Grid.col [] [ index model ] ]
+                div [] <| index model
 
             Post post ->
                 let
                     contents =
-                        case post.contents of
-                            Just c ->
-                                c
-
-                            Nothing ->
-                                "No contents!"
+                        Maybe.withDefault "No contents!" post.contents
                 in
                     Grid.row []
                         [ Grid.col []
